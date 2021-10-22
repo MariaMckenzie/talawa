deed65ce845d002cf0d9c64f38df3f0b0a7e84f3
Michaela Wright <75323745+Mich-moon@users.noreply.github.com>
2021-10-21 22:06:18 -0500

    added comments for class and its internals

--- lib/models/chats/chat_user.dart
+++ lib/models/chats/chat_user.dart
@@ -1,15 +1,26 @@
 import 'package:json_annotation/json_annotation.dart';
 part 'chat_user.g.dart';
 
+/// A user that engages in chat messaging
 @JsonSerializable()
 class ChatUser {
-  ChatUser({this.name, this.id, this.image});
+  /// The generated code below executes if the corresponding JSON value doesn't
+  /// exist or is empty.
 
-  factory ChatUser.fromJson(Map<String, dynamic> json) =>
-      _$ChatUserFromJson(json);
-  Map<String, dynamic> toJson() => _$ChatUserToJson(this);
-
+  /// Nullable instance properties
   String? name;
   String? id;
   String? image;
+
+  /// Returns a new instance of ChatUser class
+  ChatUser({this.name, this.id, this.image});
+
+  /// Connects the generated [_$ChatUserFromJson] function to the `fromJson` factory.
+  factory ChatUser.fromJson(Map<String, dynamic> json) =>
+      _$ChatUserFromJson(json);
+
+  /// Connects the generated [_$ChatUserToJson] function to the `toJson` method. 
+  Map<String, dynamic> toJson() => _$ChatUserToJson(this);
+
+
 }
